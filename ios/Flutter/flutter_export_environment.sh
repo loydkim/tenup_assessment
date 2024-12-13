de437641bf02253b55e62da18d764a1dc039c20b
Loydkim <kimloydmail@gmail.com>
2024-12-12 16:22:16 -0800

    Add files via upload

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/loydlab/Documents/FlutterSDK"
+export "FLUTTER_APPLICATION_PATH=/Users/loydlab/Documents/Personal_Projects/10up_project/tenup_project"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/loydlab/Documents/Personal_Projects/10up_project/tenup_project/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/loydlab/Documents/Personal_Projects/10up_project/tenup_project/.dart_tool/package_config.json"
